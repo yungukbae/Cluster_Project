0eab9574bf95e9060394099d68d7c40c0e8e8313
yungukbae <yungukbae@gmail.com>
2021-08-12 23:54:15 +0900

    mod: Auth_Form.css

--- src/component/Loading_Page.js
+++ src/component/Loading_Page.js
@@ -4,12 +4,9 @@ const Loading_Page = () => {
     return(
         <div>
         <div className="container">
-            <div style={{width:'200px', height:'100vh',margin:'0 auto'}}>
-                <div style={{height:'10vh',paddingTop:'45vh',textAlign:'center',}}>
-                    <p className="h1">CLUSTER</p>
-                    <p className="text-muted">개발자들의 커뮤니티</p>
-                </div>
-            </div>
+
+            CLUSTER
+
         </div>
         </div>
     )
