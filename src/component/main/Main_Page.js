78f05ba50a44f68b577dd84ca490016d9dc15523
yungukbae <yungukbae@gmail.com>
2021-08-14 14:38:36 +0900

    add: responsive 1024

--- src/component/main/Main_Page.js
+++ src/component/main/Main_Page.js
@@ -91,6 +91,14 @@ const Main_Page = () => {
                                 <div className="group_info"><p>저희는 React라이브러리를 스터디하는 그룹입니다. 주로 홍대입구역에 위치한 커피숍에서 스터디를 하고 각자 집으로 흩어집니다. 총 기간은 3개월 생각중이며 끝까지 함께하신 그룹원을 구합니다. 감사합니다. 많은 지원 바랍니다.</p></div>
                             </div>
                         </div>
+                        <div className="group_block next_arrow">
+                            <div className="picture_circle"><img src="/img/react.png" alt="no"/></div>
+                            <div className="group_content">
+                                <div className="group_title">이건 Next 버튼</div>
+                                <div className="group_type">스터디</div>
+                                <div className="group_info"><p>저희는 React라이브러리를 스터디하는 그룹입니다. 주로 홍대입구역에 위치한 커피숍에서 스터디를 하고 각자 집으로 흩어집니다. 총 기간은 3개월 생각중이며 끝까지 함께하신 그룹원을 구합니다. 감사합니다. 많은 지원 바랍니다.</p></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
