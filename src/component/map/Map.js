436660c5f3f3f35ec8026269bac1bfdb7ffa4c7c
yungukbae <yungukbae@gmail.com>
2021-09-02 22:59:23 +0900

    add: kakao map

--- src/component/map/Map.js
+++ src/component/map/Map.js
@@ -1,16 +1,16 @@
-import {useEffect, useRef} from "react";
+import {useEffect} from "react";
 
 const Map = () => {
 
     useEffect(() => {
-        var container = document.getElementById('map');
-        var options = {
+        let container = document.getElementById('map');
+        let options = {
             center: new window.kakao.maps.LatLng(37.365264512305174, 127.10676860117488),
             level: 3
         };
-        var map = new window.kakao.maps.Map(container, options);
+        let map = new window.kakao.maps.Map(container, options);
     }, []);
-//test
+
     return(
         <div>
             <div id="map" style={{width:400,height:400}}></div>
