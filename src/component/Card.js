a2c4c495954fc46dfb971e89b366f22ea7146ac3
yungukbae <yungukbae@gmail.com>
2021-09-20 01:14:47 +0900

    add: card style

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,15 +1,78 @@
+import styled from 'styled-components'
 
+const CardItem = styled.div`
 
+    width:100%;
+    height:400px;
+    display:grid;
+    grid-template-rows:350px 50px;
+  
+    border-radius:10px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+    
+    overflow:hidden;
+`
+
+const CardTop = styled.div`
+    
+    background-color:#b6b6b6;
+    position:relative;
+    
+`
+
+const CardIMG = styled.div`
+    
+    height:140px;
+    width:140px;
+    position:absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color:#fff;
+    border-radius:70px;
+    
+`
+
+const CardType = styled.div`
+    
+    height: calc(100% - 60px);
+    line-height:30px;
+    
+`
+
+const CardTitle = styled.div`
+
+    height:40px;
+    line-height:40px;
+
+`
 
 const Card = () => {
 
 
-    return(
-        <div>
-            card
-        </div>
-    )
+    return (
+        <div style={{width: '100%', height: '100%'}}>
+            <CardItem>
+                <CardTop>
+                    {/*background*/}
+                    <div style={{backgroundColor:'#000',width:'100%',height:'50%'}}>
+                        <div>
+                            <button>button</button>
+                        </div>
+                    </div>
 
+                    {/*content*/}
+                    <div style={{height:'50%'}}>
+                        <CardIMG>picture</CardIMG>
+                        <CardType>스터디</CardType>
+                        <CardTitle>이것은 제목이다.</CardTitle>
+                    </div>
+                </CardTop>
+
+                <div></div>
+            </CardItem>
+        </div>
+    );
 }
 
 export default Card;
--- src/component/GroupList.js
+++ src/component/GroupList.js
@@ -4,18 +4,27 @@ import styled from "styled-components";
 
 
 const ListContainer = styled.div`
+     display:grid;
+     width:100%;
+     height:100%;
+     grid-gap: 20px;
+     grid-template-rows:repeat(2,1fr);
+     grid-template-columns:repeat(3,1fr);
      
-`
-
-const ItemCard = styled.div`
-    
 `
 
 const GroupList = () => {
 
     return(
-        <div>
-            <Card></Card>
+        <div style={{width:'60%',height:'100vh',padding:'0 20% 0 20%'}}>
+            <ListContainer>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+             </ListContainer>
         </div>
     )
 }
