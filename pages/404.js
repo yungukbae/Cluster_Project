0ba1f2f578406469ae79bca50767bc3c439b5941
yungukbae <yungukbae@gmail.com>
2021-11-12 16:35:49 +0900

    add: 404page publishing

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,23 @@
+import styles from '../styles/404.module.css'
+
 const Custom404 = () => {
 
 
 
     return(
         <>
-
-            404
-
+            <div className={styles.topcontainer}>
+                <div className={styles.middlelogo}>
+                    <div className={styles.ddudo}>DDUDO</div>
+                    <div className={styles.ddudodes}>뚜두와 함께 시작하세요!</div>
+                </div>
+            </div>
+            <div className={styles.middlecontainer}>
+                <div className={styles.wrongtext}>404!</div>
+                <div className={styles.wronglogo}><img src="./404_logo_svg/wrong_sign.svg" alt="No Img" /></div>
+                <div className={styles.infotext}> 잘못된 경로 입니다.</div>
+                <div className={styles.timetext}>5초후 메인 페이지로 이동됩니다...</div>
+            </div>
         </>
     )
 }
